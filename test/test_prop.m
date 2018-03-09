526c9cdd9830ef3f91032e3b87d285e863ea85a1
gpavanb <pavang@stanford.edu>
2018-03-09 15:15:00 -0800

    Added CorrelationProp submodule and test

---
+++ test/test_prop.m
@@ -0,0 +1,27 @@
+clc; clear all; close all;
+
+% ADD PATHS
+addpath('../include');
+
+% PALETTE AND INITIAL LIST MUST BE INPUT FOR CORRELATION MODE
+% palette = funcName;
+% initList = initName;
+palette = {'IC8H18','C7H8','C7H16'};
+initList = [0.56,0.28,0.16];
+p = 101325; % Pa
+T = 300; % K
+
+fuel = CorrelationProp(palette,initList);
+
+% PRINT ALL PROPERTIES
+disp(palette);
+fprintf('MW = %s\n',sprintf('%.4g ',fuel.MW));
+fprintf('Tc = %s\n',sprintf('%.4g ',fuel.TcVec));
+fprintf('Acentric = %s\n',sprintf('%.4g ',fuel.omegaVec));
+fprintf('Latent Heat = %s\n',sprintf('%.4g ',fuel.L(T)));
+fprintf('Specific Heat = %s\n',sprintf('%.4g ',fuel.c_l(T)));
+fprintf('Diffusivity = %s\n',sprintf('%.4g ',fuel.D(p,T)));
+fprintf('Saturated Vapor Pressure = %s\n',sprintf('%.4g ',fuel.Psat(T)));
+fprintf('Thermal Conductivity = %s\n',sprintf('%.4g ',fuel.lambdaL(T)));
+fprintf('Surface Tension = %s\n',sprintf('%.4g ',fuel.sigma(T)));
+
